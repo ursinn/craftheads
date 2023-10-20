0c33fcca58a4ec67fa1bf50c50b563cbfa53dd71
Ursin Filli <git@ursinn.dev>
2023-10-20 23:46:07 +0200

    Replace UpdateChecker
    (cherry picked from commit aa32313b6a0fd3115a37771a2baab656cd5f1aae)

--- src/main/java/me/deejayarroba/craftheads/listeners/PlayerJoin.java
+++ src/main/java/me/deejayarroba/craftheads/listeners/PlayerJoin.java
@@ -2,6 +2,7 @@ package me.deejayarroba.craftheads.listeners;
 
 import me.deejayarroba.craftheads.Main;
 import me.deejayarroba.craftheads.utils.MessageManager;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
@@ -9,15 +10,26 @@ import org.bukkit.event.player.PlayerJoinEvent;
 public class PlayerJoin implements Listener {
 
     @EventHandler
-    public void onJoin(PlayerJoinEvent e) {
+    public void onJoin(PlayerJoinEvent event) {
+        Player player = event.getPlayer();
+
         if (!Main.devBuild) {
-            if (Main.getInstance().getConfig().getBoolean("update-check")) {
-                if (e.getPlayer().hasPermission("craftheads.updater") || e.getPlayer().isOp()) {
-                    if (Main.getInstance().updateChecker.isUpdate())
-                        MessageManager.getInstance().info(e.getPlayer(), Main.getLanguage().getLanguageConfig().getString("update.notify", "An update for CraftHeads is available"));
-                }
-            }
+            return;
         }
+
+        if (!Main.getInstance().getConfig().getBoolean("update-check")) {
+            return;
+        }
+
+        if (!player.hasPermission("craftheads.updater") && !player.isOp()) {
+            return;
+        }
+
+        if (!Main.getInstance().getUpdateChecker().isUpdateAvailable()) {
+            return;
+        }
+
+        MessageManager.getInstance().info(player, Main.getLanguage().getLanguageConfig().getString("update.notify", "An update for CraftHeads is available"));
     }
 
 }
